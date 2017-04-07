0e538de21f7c791efe7277f1e8587d489774e6cd
Onur Ozay <oonurozay@gmail.com>
2017-04-07 00:06:54 -0700

    Add more fields to cpf

--- cfp/views.py
+++ cfp/views.py
@@ -4,4 +4,19 @@ from cfp.models import Proposal
 class ProposalCreate(CreateView):
     model = Proposal
     template_name = 'cfp.html'
-    fields = ['first_name', 'last_name', 'category']
+    fields = ['first_name', 
+              'last_name',
+              'email',
+              'profile_link',
+              'phone',
+              'monthly_talk', 
+              'category',
+              'speaker_bio',
+             'talk_title',
+             'audience_level',
+             'description',
+             'abstract',
+             'outcomes',
+             'talk_history',
+             'file_submission',
+             'prior_talks']
