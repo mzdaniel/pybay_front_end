133621335a5db6bdb1326251cc64cc771cee877e
Onur Ozay <oonurozay@gmail.com>
2017-04-12 13:17:36 -0700

    revise templates

--- urls.py
+++ urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^cfp($|/)', ProposalCreate.as_view(success_url='/'), name="cfp"),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name="main"),
+    url(r'^sponsor/$', TemplateView.as_view(template_name='sponsor.html'), name="sponsor"),
 ]
